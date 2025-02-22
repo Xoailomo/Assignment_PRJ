f2bc5f9c164f5dc3cda7002be54483dc033cc2f5
Mango <phankhoatn2011@gmail.com>
2025-02-22 21:04:16 +0700

    first: add DBContext base class for database operations
    - Added abstract class DBContext<T> to handle database connection.
    - Implemented JDBC connection using SQL Server.
    - Defined abstract CRUD methods for subclasses.

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sonnt-local
+ */
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "sa";
+            String pass = "sa";
+            String url = "jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=IS1903;trustServerCertificate=true; [sa on SA]";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public abstract ArrayList<T> list();
+    public abstract T get(int id);
+    public abstract void insert(T model);
+    public abstract void update(T model);
+    public abstract void delete(T model);
+    
+}

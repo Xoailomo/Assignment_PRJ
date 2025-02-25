1c69e941653c1cdc91a6b8fbf12c03e45abc5de0
Mango <phankhoatn2011@gmail.com>
2025-02-25 16:19:52 +0700

    feat: add model.Role

---
+++ src/java/model/Role.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author phank
+ */
+public class Role {
+    private int id;
+    private String name;
+//
+//    public Role(int id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+
+    // Getters và setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}

18ea96a13208a27fffb144d5ae93d0a689f16aaa
Mango <phankhoatn2011@gmail.com>
2025-02-25 16:24:34 +0700

    fix: variable username

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -10,7 +10,7 @@ package model;
  */
 public class User {
     private int id;
-    private String name;
+    private String username;
     private String email;
     private String password;
     private Department department;
@@ -18,8 +18,8 @@ public class User {
 
    public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() { return username; }
+    public void setName(String name) { this.username = name; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }

457aed48b50a45917ecf13bf3094202a80cb54f7
Mango <phankhoatn2011@gmail.com>
2025-02-25 16:28:28 +0700

    fix getter and setter

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -19,7 +19,7 @@ public class User {
    public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getName() { return username; }
-    public void setName(String name) { this.username = name; }
+    public void setUserName(String name) { this.username = name; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }

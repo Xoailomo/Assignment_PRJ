a1df720086b0fb7ad9ea9c9cc3d7e07b2b362b82
Mango <phankhoatn2011@gmail.com>
2025-03-01 22:32:02 +0700

    fix: add dept and managerId in model User

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -1,22 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author sonnt-local
- */
 public class User {
     private int id;
     private String username;
     private String email;
     private String password;
     private Department department;
-    
+    private Integer managerId; // Sử dụng Integer để cho phép NULL (nhân viên cấp cao nhất không có quản lý)
 
-   public int getId() { return id; }
+    public int getId() { return id; }
     public void setId(int id) { this.id = id; }
     public String getName() { return username; }
     public void setUserName(String name) { this.username = name; }
@@ -26,4 +18,6 @@ public class User {
     public void setPassword(String password) { this.password = password; }
     public Department getDepartment() { return department; }
     public void setDepartment(Department department) { this.department = department; }
+    public Integer getManagerId() { return managerId; } // Getter trả về Integer
+    public void setManagerId(Integer managerId) { this.managerId = managerId; } // Setter nhận Integer
 }

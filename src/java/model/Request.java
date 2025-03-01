e8a8b080304f32155db032c940a57495be589612
Mango <phankhoatn2011@gmail.com>
2025-03-01 23:08:09 +0700

    update request model

--- src/java/model/Request.java
+++ src/java/model/Request.java
@@ -3,33 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
+import java.sql.Timestamp;
 import java.sql.Date;
 /**
  *
  * @author phank
  */
 public class Request {
-    private int id;
+    private int requestId;
     private int userId;
     private Date fromDate;
     private Date toDate;
     private String reason;
     private String status;
-    private int processedBy;
+    private Integer processedBy; // NULLable
+    private Timestamp createdAt;
+    private int createdBy;
+    private User user; // Để lưu thông tin user
 
-//    public Request(int id, int userId, Date fromDate, Date toDate, String reason, String status, int processedBy) {
-//        this.id = id;
-//        this.userId = userId;
-//        this.fromDate = fromDate;
-//        this.toDate = toDate;
-//        this.reason = reason;
-//        this.status = status;
-//        this.processedBy = processedBy;
-//    }
-
-    // Getters và setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    // Getters and Setters
+    public int getRequestId() { return requestId; }
+    public void setRequestId(int requestId) { this.requestId = requestId; }
     public int getUserId() { return userId; }
     public void setUserId(int userId) { this.userId = userId; }
     public Date getFromDate() { return fromDate; }
@@ -40,6 +34,12 @@ public class Request {
     public void setReason(String reason) { this.reason = reason; }
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
-    public int getProcessedBy() { return processedBy; }
-    public void setProcessedBy(int processedBy) { this.processedBy = processedBy; }
+    public Integer getProcessedBy() { return processedBy; }
+    public void setProcessedBy(Integer processedBy) { this.processedBy = processedBy; }
+    public Timestamp getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Timestamp createdAt) { this.createdAt = createdAt; }
+    public int getCreatedBy() { return createdBy; }
+    public void setCreatedBy(int createdBy) { this.createdBy = createdBy; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 }

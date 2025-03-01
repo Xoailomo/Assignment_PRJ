7de9609f9bd7ea973bf656e0360e4d8fda7126ad
Mango <phankhoatn2011@gmail.com>
2025-03-01 22:31:41 +0700

    fix: add description to model Role

--- src/java/model/Role.java
+++ src/java/model/Role.java
@@ -9,17 +9,38 @@ package model;
  * @author phank
  */
 public class Role {
+
     private int id;
     private String name;
+    private String Description;
 //
 //    public Role(int id, String name) {
 //        this.id = id;
 //        this.name = name;
 //    }
 
+    public String getDescription() {
+        return Description;
+    }
+
     // Getters và setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }

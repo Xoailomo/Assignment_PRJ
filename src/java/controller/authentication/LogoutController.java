3968d37704a8c063be7d95b7eb770dd3bb2a520f
Mango <phankhoatn2011@gmail.com>
2025-03-04 14:42:38 +0700

    feat: add dashboard

---
+++ src/java/controller/authentication/LogoutController.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.authentication;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author phank
+ */
+public class LogoutController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath()+"/login");
+    }
+   
+    
+}

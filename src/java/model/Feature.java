2e00f7191dcd67506d19384d05c8da6c55606968
Mango <phankhoatn2011@gmail.com>
2025-03-04 14:53:02 +0700

    update feature model
    - Add arraylist roles, string url, and theri getter & setter

--- src/java/model/Feature.java
+++ src/java/model/Feature.java
@@ -4,26 +4,57 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author phank
  */
 public class Feature {
+
     private int id;
     private String name;
     private String description;
+    private String url;
+    private ArrayList<Role> roles = new ArrayList<>();
 
-//    public Feature(int id, String name, String description) {
-//        this.id = id;
-//        this.name = name;
-//        this.description = description;
-//    }
+    public ArrayList<Role> getRoles() {
+        return roles;
+    }
 
-    // Getters và setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
+    public void setRoles(ArrayList<Role> roles) {
+        this.roles = roles;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

045e607c05277cff459773a307ad687fd1333b4b
Mango <phankhoatn2011@gmail.com>
2025-03-04 18:37:01 +0700

    update controller auth
    - Add isAllowAcess to file BaseRequireAuthenticationController.java

--- src/java/controller/authentication/BaseRequiredAuthenticationController.java
+++ src/java/controller/authentication/BaseRequiredAuthenticationController.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import model.Feature;
+import model.Role;
 import model.User;
 
 /**
@@ -21,10 +23,22 @@ public abstract class BaseRequiredAuthenticationController extends HttpServlet {
         return (User) req.getSession().getAttribute("user");
     }
 
+    private boolean isAllowedAccess(User u, HttpServletRequest req) {
+        String current_endpoint = req.getServletPath();
+        for (Role role : u.getRoles()) {
+            for (Feature feature : role.getFeatures()) {
+                if (feature.getUrl().equals(current_endpoint)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User u = getLoggedUser(req);
-        if (u != null) {
+        if (u != null && isAllowedAccess(u, req)) {
             //do business
             doPost(req, resp, u);
         } else {
@@ -39,7 +53,7 @@ public abstract class BaseRequiredAuthenticationController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User u = getLoggedUser(req);
-        if (u != null) {
+        if (u != null && isAllowedAccess(u, req)) {
             //do business
             doGet(req, resp, u);
         } else {

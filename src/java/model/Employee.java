16137a791723ecfdbc2903e6fbac0d80d1a49698
Mango <phankhoatn2011@gmail.com>
2025-03-04 14:53:51 +0700

    add employee model

--- src/java/model/Employee.java
+++ src/java/model/Employee.java
@@ -4,10 +4,66 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author phank
+ * @author sonnt-local
  */
 public class Employee {
+    private int id;
+    private String name;
+    private Employee manager;
+    private Department dept;
+    private ArrayList<Employee> staffs = new ArrayList<>();
+    private ArrayList<Employee> directstaffs = new ArrayList<>();
+
+    public ArrayList<Employee> getDirectstaffs() {
+        return directstaffs;
+    }
+
+    public void setDirectstaffs(ArrayList<Employee> directstaffs) {
+        this.directstaffs = directstaffs;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Employee getManager() {
+        return manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public Department getDept() {
+        return dept;
+    }
+
+    public void setDept(Department dept) {
+        this.dept = dept;
+    }
+
+    public ArrayList<Employee> getStaffs() {
+        return staffs;
+    }
+
+    public void setStaffs(ArrayList<Employee> staffs) {
+        this.staffs = staffs;
+    }
     
 }

2a0d06db85b83ee139acc37f707556fdcf38b431
Mango <phankhoatn2011@gmail.com>
2025-03-04 14:57:41 +0700

    feat: add controller dashboard

---
+++ src/java/controller/dashboard/DashboardController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.dashboard;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import model.User;
+
+/**
+ *
+ * @author phank
+ */
+public class DashboardController extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session == null || session.getAttribute("user") == null){
+            resp.sendRedirect(req.getContextPath()+"/login");
+            return;
+        }
+        User user = (User) + session.getAttribute("user");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+    
+    
+}

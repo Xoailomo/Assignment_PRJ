ad4f25eb6f1936e62966729826a05ad3f3d19b6a
Mango <phankhoatn2011@gmail.com>
2025-03-04 16:30:31 +0700

    add LeaveRequest
    - use lecture sample

---
+++ src/java/model/LeaveRequest.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author phank
+ */
+public class LeaveRequest {
+    private String title;
+    private String reason;
+    private Date from;
+    private Date to;
+    private int status;
+    private Employee owner;
+    private User processedby;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Employee getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Employee owner) {
+        this.owner = owner;
+    }
+
+    public User getProcessedby() {
+        return processedby;
+    }
+
+    public void setProcessedby(User processedby) {
+        this.processedby = processedby;
+    }
+    
+}

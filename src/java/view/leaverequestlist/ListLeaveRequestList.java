6678a8d35fbf91d33233805bd2ca639ba7f71b53
Mango <phankhoatn2011@gmail.com>
2025-03-08 11:59:16 +0700

    add request url for listLeaveRequest controller

--- src/java/view/leaverequestlist/ListLeaveRequestList.java
+++ src/java/view/leaverequestlist/ListLeaveRequestList.java
@@ -30,7 +30,7 @@ public class ListLeaveRequestList extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+        req.getRequestDispatcher("../view/leaverequest/request_list.jsp").forward(req, resp);
     }
    
    

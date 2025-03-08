3cc55a8c2939c0cf61f806e278307f72d266bf9f
Mango <phankhoatn2011@gmail.com>
2025-03-08 11:35:04 +0700

    test logout
    - try to logout and the user cannot access or turn back to the previous page

--- src/java/controller/authentication/LogoutController.java
+++ src/java/controller/authentication/LogoutController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller.authentication;
 
 import java.io.IOException;
@@ -21,12 +20,13 @@ public class LogoutController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession(false);
-        if(session != null){
-            session.invalidate();
-        }
-        resp.sendRedirect(req.getContextPath()+"/login");
+       //get current session
+        HttpSession session = req.getSession();
+        // clear
+        session.invalidate();
+        resp.sendRedirect("index.jsp");
     }
-   
+
     
+
 }

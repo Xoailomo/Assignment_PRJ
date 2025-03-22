8af496690fac676e6f401369ad522ad3fcbd453c
Mango <phankhoatn2011@gmail.com>
2025-03-22 19:10:24 +0700

    Test database in LeaveManagementApplication.java
    -

--- src/main/java/com/mycompany/LeaveManagementSystem/LeaveManagementSystemApplication.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/LeaveManagementSystemApplication.java
@@ -1,11 +1,29 @@
 package com.mycompany.LeaveManagementSystem;
 
+import com.mycompany.LeaveManagementSystem.model.User;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class LeaveManagementSystemApplication {
+public class LeaveManagementSystemApplication implements CommandLineRunner {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    
     public static void main(String[] args) {
         SpringApplication.run(LeaveManagementSystemApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String sql ="select * from Users";
+        List<User> users=jdbcTemplate.query(sql, 
+                BeanPropertyRowMapper.newInstance(User.class));
+        users.forEach(System.out :: println );
+        
     }
 }

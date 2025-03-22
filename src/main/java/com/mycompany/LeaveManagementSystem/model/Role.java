66f3c68886e4b4ecfba4296a4a02758796039a58
Mango <phankhoatn2011@gmail.com>
2025-03-23 00:01:01 +0700

    Update Role.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/Role.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/Role.java
@@ -2,35 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.LeaveManagementSystem.model;
+
+import jakarta.persistence.*;
 
 /**
  *
  * @author phank
+*
  */
-
-import jakarta.persistence.*;
-import java.util.Set;
-
 @Entity
-@Table(name = "Roles")
+@Table(name = "roles")
 public class Role {
-    @Id
+@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
 
-    @Column(name = "name", nullable = false, length = 255)
+    @Column( nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<UserRole> userRoles;
-
-    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<RoleFeature> roleFeatures;
-
-    // Getter và Setter cho id
     public int getId() {
         return id;
     }
@@ -39,7 +29,6 @@ public class Role {
         this.id = id;
     }
 
-    // Getter và Setter cho name
     public String getName() {
         return name;
     }
@@ -47,22 +36,5 @@ public class Role {
     public void setName(String name) {
         this.name = name;
     }
-
-    // Getter và Setter cho userRoles
-    public Set<UserRole> getUserRoles() {
-        return userRoles;
-    }
-
-    public void setUserRoles(Set<UserRole> userRoles) {
-        this.userRoles = userRoles;
-    }
-
-    // Getter và Setter cho roleFeatures
-    public Set<RoleFeature> getRoleFeatures() {
-        return roleFeatures;
-    }
-
-    public void setRoleFeatures(Set<RoleFeature> roleFeatures) {
-        this.roleFeatures = roleFeatures;
-    }
+    
 }

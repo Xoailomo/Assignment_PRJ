b9e772830168afefbdcc56a60bc0ac8fe3d5e980
Mango <phankhoatn2011@gmail.com>
2025-03-23 00:00:07 +0700

    Update LeaveManagementSystemApplication.java
    - comment test

--- src/main/java/com/mycompany/LeaveManagementSystem/LeaveManagementSystemApplication.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/LeaveManagementSystemApplication.java
@@ -1,29 +1,29 @@
 package com.mycompany.LeaveManagementSystem;
 
-import com.mycompany.LeaveManagementSystem.model.User;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.jdbc.core.BeanPropertyRowMapper;
+//import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class LeaveManagementSystemApplication implements CommandLineRunner {
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+//public class LeaveManagementSystemApplication implements CommandLineRunner {
+public class LeaveManagementSystemApplication {
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
     
     public static void main(String[] args) {
         SpringApplication.run(LeaveManagementSystemApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        String sql ="select * from Users";
-        List<User> users=jdbcTemplate.query(sql, 
-                BeanPropertyRowMapper.newInstance(User.class));
-        users.forEach(System.out :: println );
-        
-    }
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String sql ="select * from Users";
+//        List<User> users=jdbcTemplate.query(sql, 
+//                BeanPropertyRowMapper.newInstance(User.class));
+//        users.forEach(System.out :: println );
+//        
+//    }
 }

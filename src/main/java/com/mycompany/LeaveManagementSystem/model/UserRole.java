ef4d87886e639a8e768ed1f4bccab02146a324a3
Mango <phankhoatn2011@gmail.com>
2025-03-22 19:12:26 +0700

    Create UserRole.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/UserRole.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.LeaveManagementSystem.model;
+
+/**
+ *
+ * @author phank
+ */
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "UserRole")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userid", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roleid", nullable = false)
+    private Role role;
+
+    // Getter và Setter cho id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter và Setter cho user
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    // Getter và Setter cho role
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}

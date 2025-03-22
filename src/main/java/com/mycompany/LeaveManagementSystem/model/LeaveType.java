7a2247bdc05e4d6c3dfcf868e0db7cd23d9161eb
Mango <phankhoatn2011@gmail.com>
2025-03-23 00:00:58 +0700

    Update LeaveType.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveType.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveType.java
@@ -2,38 +2,24 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.LeaveManagementSystem.model;
+
+import jakarta.persistence.*;
 
 /**
  *
  * @author phank
  */
-
-import jakarta.persistence.*;
-import java.util.Set;
-
 @Entity
-@Table(name = "LeaveType")
+@Table(name = "leave_types")
 public class LeaveType {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private int id;
+    private String name;//vacation, sick, unpaid leave
+    private String description;
 
-    @Column(name = "name", nullable = false, length = 255)
-    private String name;
-
-    @OneToMany(mappedBy = "leaveType", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<LeaveBalance> leaveBalances;
-
-    @OneToMany(mappedBy = "leaveType", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<LeaveEntitlement> leaveEntitlements;
-
-    @OneToMany(mappedBy = "leaveType", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<LeaveRequest> leaveRequests;
-
-    // Getter và Setter cho id
     public int getId() {
         return id;
     }
@@ -42,7 +28,6 @@ public class LeaveType {
         this.id = id;
     }
 
-    // Getter và Setter cho name
     public String getName() {
         return name;
     }
@@ -51,30 +36,12 @@ public class LeaveType {
         this.name = name;
     }
 
-    // Getter và Setter cho leaveBalances
-    public Set<LeaveBalance> getLeaveBalances() {
-        return leaveBalances;
+    public String getDescription() {
+        return description;
     }
 
-    public void setLeaveBalances(Set<LeaveBalance> leaveBalances) {
-        this.leaveBalances = leaveBalances;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
-    // Getter và Setter cho leaveEntitlements
-    public Set<LeaveEntitlement> getLeaveEntitlements() {
-        return leaveEntitlements;
-    }
-
-    public void setLeaveEntitlements(Set<LeaveEntitlement> leaveEntitlements) {
-        this.leaveEntitlements = leaveEntitlements;
-    }
-
-    // Getter và Setter cho leaveRequests
-    public Set<LeaveRequest> getLeaveRequests() {
-        return leaveRequests;
-    }
-
-    public void setLeaveRequests(Set<LeaveRequest> leaveRequests) {
-        this.leaveRequests = leaveRequests;
-    }
 }

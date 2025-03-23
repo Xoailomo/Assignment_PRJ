3664f065ad907917d4099e6783c86ddf85090322
Mango <phankhoatn2011@gmail.com>
2025-03-23 15:19:00 +0700

    Update LeaveRequest.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
@@ -23,12 +23,12 @@ public class LeaveRequest {
     @JoinColumn(nullable = false)
     private Employee employee;
 
-    @ManyToOne
-    @JoinColumn(nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private LeaveType leaveType;
-
+    @Column(nullable = false)
     private LocalDate startDate;
-
+    @Column(nullable = false)
     private LocalDate endDate;
 
     @Enumerated(EnumType.STRING)

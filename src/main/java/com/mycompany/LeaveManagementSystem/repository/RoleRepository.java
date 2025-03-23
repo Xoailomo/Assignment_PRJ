23014cfd250fe24a9a50e3872803093b64aee78f
Mango <phankhoatn2011@gmail.com>
2025-03-24 02:42:59 +0700

    Update RoleRepository.java
    Change Long to Integer

--- src/main/java/com/mycompany/LeaveManagementSystem/repository/RoleRepository.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/repository/RoleRepository.java
@@ -4,6 +4,6 @@ import com.mycompany.LeaveManagementSystem.model.Role;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Integer> {
     Optional<Role> findByName(String name); 
 }

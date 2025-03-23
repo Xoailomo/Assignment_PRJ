5b6e3d40a713faf2ae37c3ffc2628d1343baf74c
Mango <phankhoatn2011@gmail.com>
2025-03-24 02:40:20 +0700

    Update HomeController.java
    -Change home to index

--- src/main/java/com/mycompany/LeaveManagementSystem/controller/HomeController.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/controller/HomeController.java
@@ -3,19 +3,20 @@ package com.mycompany.LeaveManagementSystem.controller;
 
 //import com.mycompany.LeaveManagementSystem.service.LeaveRequestService;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 //    @Autowired
 //    private LeaveRequestService service;
 
     @GetMapping("/")
-    public String home() {
+    public String index() {
         return "index";
     }
     @GetMapping("/admin/home")

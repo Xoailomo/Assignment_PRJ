8948b0f207daf9e7e60cf65556ba9790636909cc
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:34:01 +0700

    Create RegisterDTO.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/dto/RegisterDTO.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+/**
+ *
+ * @author phank
+ */
+public class RegisterDTO {
+    @NotEmpty
+    private String firstname;
+    @NotEmpty
+    private String lastname;
+    @NotEmpty
+    private String username;
+
+    private String phone;
+    @NotEmpty
+    private String email;
+
+    private String address;
+    @NotEmpty
+    @Size(min=6, message = "Minimum password length is 6 characters")
+    private String password;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+
+    // Getters và Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}

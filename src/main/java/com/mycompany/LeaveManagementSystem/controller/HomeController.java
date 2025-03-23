fb85ce899177fdc2730de90878d0c98f4d336e7f
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:31:17 +0700

    Update HomeController.java

--- src/main/java/com/mycompany/LeaveManagementSystem/controller/HomeController.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/controller/HomeController.java
@@ -2,13 +2,13 @@ package com.mycompany.LeaveManagementSystem.controller;
 
 
 //import com.mycompany.LeaveManagementSystem.service.LeaveRequestService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
 //    @Autowired
@@ -18,10 +18,23 @@ public class HomeController {
     public String home() {
         return "index";
     }
-    @GetMapping("/create")
+    @GetMapping("/admin/home")
+    public String getAdminHome() {
+        return "Admin Home Page";
+    }
+    @GetMapping("/manager/home")
+    public String getManagerHome() {
+        return "Manager Home Page";
+    }
+    @GetMapping("/staff/home")
+    public String getStaffHome() {
+        return "Staff Home Page";
+    }
+    @GetMapping("/create-request")
     public String create() {
         return "create";
     }
+    
 
 //    @PostMapping("/create-request")
 //    public String createRequest(LeaveRequest request, @RequestParam(required = false) Integer ownerId) {

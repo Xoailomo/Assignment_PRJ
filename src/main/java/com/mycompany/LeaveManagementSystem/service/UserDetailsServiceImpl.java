aa52c3f5cdaaf4295059edfd192866a1add6b534
Mango <phankhoatn2011@gmail.com>
2025-03-24 02:43:29 +0700

    Create UserDetailsServiceImpl.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/service/UserDetailsServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.service;
+
+
+import com.mycompany.LeaveManagementSystem.model.Users;
+import com.mycompany.LeaveManagementSystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author phank
+ */
+@Service
+@Primary  // Đánh dấu bean này là ưu tiên
+public class UserDetailsServiceImpl implements UserDetailsService {
+//
+    @Autowired
+    private UserRepository userRepository;  
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepository.findByUsername(username);
+
+        if (user != null) {
+            var springUser = User.withUsername(user.getUsername())
+                    .password(user.getPassword())
+                    .roles(user.getRole().split(",")) // Truyền role vào đây
+                    .build();
+            return springUser;
+        }
+        return null;
+
+    }
+}

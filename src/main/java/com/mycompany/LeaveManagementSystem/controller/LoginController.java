9d77dccc6430ecc956005fdfde384b2f6ea0117c
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:31:36 +0700

    Update LoginController.java

--- src/main/java/com/mycompany/LeaveManagementSystem/controller/LoginController.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/controller/LoginController.java
@@ -1,21 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.LeaveManagementSystem.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.mycompany.LeaveManagementSystem.model.Users;
+import com.mycompany.LeaveManagementSystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
-/**
- *
- * @author phank
- */
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
 public class LoginController {
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login"; // Trả về trang login.jsp hoặc login.html
-    }
-}
 
+//    private final AuthenticationManager authenticationManager;
+////    private final JwtUtil jwtUtil;
+//    private final UserService userService;
+//
+////    @Autowired
+////    public LoginController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserService userService) {
+////        this.authenticationManager = authenticationManager;
+////        this.jwtUtil = jwtUtil;
+////        this.userService = userService;
+////    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
+//        String username = loginRequest.get("username");
+//        String password = loginRequest.get("password");
+//
+//        // Xác thực user
+//        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+//
+//        // Nếu xác thực thành công, lấy user từ DB
+//        Users user = userService.getUserByUsername(username);
+//        String token = jwtUtil.generateToken(user.getUsername());
+//
+//        Map<String, String> response = new HashMap<>();
+//        response.put("token", token);
+//        return ResponseEntity.ok(response);
+//    }
+}

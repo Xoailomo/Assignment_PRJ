ed112ffaeb25f0ff8025300d06c3f0136ace236a
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:31:46 +0700

    Update UserController.java

--- src/main/java/com/mycompany/LeaveManagementSystem/controller/UserController.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/controller/UserController.java
@@ -1,10 +1,7 @@
 package com.mycompany.LeaveManagementSystem.controller;
 
-import com.mycompany.LeaveManagementSystem.model.User;
+import com.mycompany.LeaveManagementSystem.model.Users;
 import com.mycompany.LeaveManagementSystem.service.UserService;
-import com.mycompany.LeaveManagementSystem.util.JwtUtil;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -15,43 +12,43 @@ public class UserController {
 
     private final UserService userService;
     private final PasswordEncoder passwordEncoder = null;
-    private final JwtUtil jwtUtil = null;
+//    private final JwtUtil jwtUtil = null;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody User user) {
-        try {
-            User newUser = userService.registerUser(
-                    user.getUsername(),
-                    user.getEmail(),
-                    user.getFirstName(),
-                    user.getLastName(),
-                    user.getDisplayName(),
-                    user.getPassword(),
-                    user.getEmployee() != null ? user.getEmployee().getId() : 0
-            );
-            return ResponseEntity.ok("User registered successfully!");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
+//    @PostMapping("/register")
+//    public ResponseEntity<String> registerUser(@RequestBody Users user) {
+//        try {
+//            Users newUser = userService.registerUser(
+//                    user.getUsername(),
+//                    user.getEmail(),
+//                    user.getFirstName(),
+//                    user.getLastName(),
+//                    user.getDisplayName(),
+//                    user.getPassword(),
+//                    user.getEmployee() != null ? user.getEmployee().getId() : 0
+//            );
+//            return ResponseEntity.ok("Users registered successfully!");
+//        } catch (Exception e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
 
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> loginRequest) {
-        String username = loginRequest.get("username");
-        String password = loginRequest.get("password");
-
-        User user = userService.getUserByUsername(username);
-        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
-            return ResponseEntity.status(401).body("Invalid credentials");
-        }
-
-        String token = jwtUtil.generateToken(username);
-        Map<String, String> response = new HashMap<>();
-        response.put("token", token);
-        return ResponseEntity.ok(response);
-    }
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> loginRequest) {
+//        String username = loginRequest.get("username");
+//        String password = loginRequest.get("password");
+//
+//        Users user = userService.getUserByUsername(username);
+//        if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+//            return ResponseEntity.status(401).body("Invalid credentials");
+//        }
+//
+////        String token = jwtUtil.generateToken(username);
+////        Map<String, String> response = new HashMap<>();
+////        response.put("token", token);
+////        return ResponseEntity.ok(response);
+//    }
 }

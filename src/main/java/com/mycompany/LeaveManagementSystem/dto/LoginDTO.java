b67f340d1e2f02147354d370b304f8efc1fdb6ed
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:34:03 +0700

    Create LoginDTO.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/dto/LoginDTO.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+/**
+ *
+ * @author phank
+ */
+public class LoginDTO {
+    @NotEmpty
+    private String username;
+    @NotEmpty
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+ 
+}

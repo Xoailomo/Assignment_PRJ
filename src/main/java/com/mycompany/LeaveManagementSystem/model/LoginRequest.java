8ed773c89a0a8fbb75bdfe5e6fbd9c32ce547985
Mango <phankhoatn2011@gmail.com>
2025-03-23 21:33:53 +0700

    Create LoginRequest.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/LoginRequest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.model;
+
+/**
+ *
+ * @author phank
+ */
+public class LoginRequest {
+    private String username;
+    private String password;
+
+    // Getters và Setters
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}

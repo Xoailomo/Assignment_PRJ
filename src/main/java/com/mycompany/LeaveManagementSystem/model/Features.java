9d0e014b79a8c696cd90da97ef8ffca70f8dbf6a
Mango <phankhoatn2011@gmail.com>
2025-03-24 23:28:47 +0700

    Create Features.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/Features.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+/**
+ *
+ * @author phank
+ */
+@Entity
+@Table(name="features")
+public class Features {
+    @
+    private int fid;
+    
+}

35a05b58c35c92ebca6e904479c5f313391cfcc4
Mango <phankhoatn2011@gmail.com>
2025-03-25 19:03:06 +0700

    Update LeaveRequest.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
@@ -6,90 +6,17 @@ package com.mycompany.LeaveManagementSystem.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
+import lombok.*;
 
 /**
  *
  * @author phank
  */
+@Getter
+@Setter
 @Entity
 @Table(name = "leave_requests")
 public class LeaveRequest {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @ManyToOne
-    @JoinColumn(nullable = false)
-    private Employee employee;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private LeaveType leaveType;
-    @Column(nullable = false)
-    private LocalDate startDate;
-    @Column(nullable = false)
-    private LocalDate endDate;
-
-    @Enumerated(EnumType.STRING)
-    private LeaveStatus status = LeaveStatus.INPROGRESS;
-
-    private String reason;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public LeaveType getLeaveType() {
-        return leaveType;
-    }
-
-    public void setLeaveType(LeaveType leaveType) {
-        this.leaveType = leaveType;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public LeaveStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(LeaveStatus status) {
-        this.status = status;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
+    @
+    
 }

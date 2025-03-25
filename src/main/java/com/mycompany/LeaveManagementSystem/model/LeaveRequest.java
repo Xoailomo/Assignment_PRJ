fe3fde299ec27b89574fad4bdf2340cda45719d9
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:33:35 +0700

    Update LeaveRequest.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/LeaveRequest.java
@@ -17,6 +17,38 @@ import lombok.*;
 @Entity
 @Table(name = "leave_requests")
 public class LeaveRequest {
-    @
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int elid;  // Mã đơn nghỉ
+
+    @ManyToOne
+    @JoinColumn(name = "eid", nullable = false)
+    private Employees employee;  // Nhân viên nghỉ phép
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)  
+    private Employees createdBy;  // Người tạo đơn (có thể là quản lý)
+
+    @ManyToOne
+    @JoinColumn(name = "processed_by")  
+    private Employees processedBy;  // Người duyệt đơn nghỉ (Manager/Admin)
+
+    @ManyToOne
+    @JoinColumn(name = "ltid", nullable = false)
+    private LeaveTypes leaveType;  // Loại nghỉ phép
+
+    @Column(nullable = false, length = 255)
+    private String reasonTitle;  // Tiêu đề lý do nghỉ
+
+    @Column(nullable = false)
+    private LocalDate  startDate;  // Ngày bắt đầu nghỉ
+
+    @Column(nullable = false)
+    private LocalDate  endDate;  // Ngày kết thúc nghỉ
+
+    @Enumerated(EnumType.STRING) // Lưu enum dạng chuỗi trong database
+    private LeaveStatus status; 
+    
+    private String reason;
     
 }

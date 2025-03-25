25b9ceccea1c8849d613041c8deea7e2a852d983
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:34:31 +0700

    Update RegisterDTO.java

--- src/main/java/com/mycompany/LeaveManagementSystem/dto/RegisterDTO.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/dto/RegisterDTO.java
@@ -7,90 +7,32 @@ package com.mycompany.LeaveManagementSystem.dto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import java.util.Date;
+import lombok.*;
 
 /**
  *
  * @author phank
  */
+@Getter
+@Setter
 public class RegisterDTO {
+
     @NotEmpty
     private String firstname;
     @NotEmpty
     private String lastname;
     @NotEmpty
     private String username;
-
     private String phone;
     @NotEmpty
     private String email;
     @NotEmpty
     private String displayName;
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-    
     @NotEmpty
     private Date createAt;
-
-    public Date getCreateAt() {
-        return createAt;
-    }
-
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
-    
-    
-
     private String address;
     @NotEmpty
-    @Size(min=6, message = "Minimum password length is 6 characters")
+    @Size(min = 6, message = "Minimum password length is 6 characters")
     private String password;
 
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
-    
-
-    // Getters và Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
 }

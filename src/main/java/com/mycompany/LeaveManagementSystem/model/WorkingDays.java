6f1e9fe183a6492b0a4ae92e0ca8cfb00b8ebe04
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:29:11 +0700

    Create WorkingDays.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/WorkingDays.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.model;
+import lombok.*;
+import jakarta.persistence.*;
+/**
+ *
+ * @author phank
+ */
+@Getter
+@Setter
+@Entity
+@Table(name = "working_days")
+public class WorkingDays {
+    @Id
+    private int wdid;
+
+    @Column(nullable = false, length = 50)
+    private String dayOfWeek;
+
+    @Column(nullable = false)
+    private boolean isWorkingDay;
+    
+    
+}

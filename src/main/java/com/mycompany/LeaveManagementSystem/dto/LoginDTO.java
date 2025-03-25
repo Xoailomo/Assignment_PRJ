6967ab12fdca3b87c39cedc24d5c9ad699155eb8
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:34:36 +0700

    Update LoginDTO.java

--- src/main/java/com/mycompany/LeaveManagementSystem/dto/LoginDTO.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/dto/LoginDTO.java
@@ -5,32 +5,18 @@
 package com.mycompany.LeaveManagementSystem.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 /**
  *
  * @author phank
  */
+@Getter
+@Setter
 public class LoginDTO {
     @NotEmpty
     private String username;
     @NotEmpty
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    
- 
 }

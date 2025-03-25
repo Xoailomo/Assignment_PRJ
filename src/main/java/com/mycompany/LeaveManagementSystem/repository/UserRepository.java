008e76194408c6bec6c8b6cdecf23e50d70a412e
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:27:17 +0700

    Update UserRepository.java

--- src/main/java/com/mycompany/LeaveManagementSystem/repository/UserRepository.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/repository/UserRepository.java
@@ -1,27 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.mycompany.LeaveManagementSystem.repository;
 
 import com.mycompany.LeaveManagementSystem.model.Users;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-/**
- *
- * @author phank
- */
-public interface UserRepository extends JpaRepository<Users, Integer>{
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, String> {
 
     Optional<Users> findByUsername(String username);
 
-    Optional<Users>  findByEmail(String email);
-//    public Users  findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 
-    public boolean existsByUsername(String username);
+    boolean existsByUsername(String username);
 
-    public boolean existsByEmail(String email);
-    
-    
+    boolean existsByEmail(String email);
+
+    // Lấy danh sách roles của user từ bảng UserDepartmentRole
+    @Query("SELECT r.rname FROM user_department_roles udr JOIN udr.role r WHERE udr.user.username = :username")
+    List<String> findRolesByUsername(@Param("username") String username);
 }

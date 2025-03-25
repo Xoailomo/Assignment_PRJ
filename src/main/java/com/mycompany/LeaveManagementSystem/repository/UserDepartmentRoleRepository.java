183e03aea130848be475ab4a1a0e49cef7811681
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:27:21 +0700

    Create UserDepartmentRoleRepository.java

---
+++ src/main/java/com/mycompany/LeaveManagementSystem/repository/UserDepartmentRoleRepository.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.LeaveManagementSystem.repository;
+
+import com.mycompany.LeaveManagementSystem.model.UserDepartmentRoleId;
+import com.mycompany.LeaveManagementSystem.model.UserDepartmentRoles;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author phank
+ */
+@Repository
+public interface UserDepartmentRoleRepository extends JpaRepository<UserDepartmentRoles, UserDepartmentRoleId> {
+
+    @Query("SELECT r.rname FROM user_department_roles udr JOIN udr.role r WHERE udr.user.username = :username")
+    List<String> findRolesByUsername(@Param("username") String username);
+}

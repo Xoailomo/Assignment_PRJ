c0595268ab49de4fe365b515449cdf44db014aae
Mango <phankhoatn2011@gmail.com>
2025-03-26 02:06:26 +0700

    Update UserDepartmentRoleId.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/UserDepartmentRoleId.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/UserDepartmentRoleId.java
@@ -13,11 +13,25 @@ import lombok.Setter;
  * @author phank
  */
 import java.io.Serializable;
+import java.util.Objects;
 
 public class UserDepartmentRoleId implements Serializable {
     private String username;
     private int did;  // ✅ Không đổi tên vì trong entity sẽ dùng department.getId()
     private int rid;
+    // Required for composite keys
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDepartmentRoleId that = (UserDepartmentRoleId) o;
+        return did == that.did && rid == that.rid && username.equals(that.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, did, rid);
+    }
 }
 
 

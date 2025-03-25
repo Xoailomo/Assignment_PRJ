da99501856dcc4f197360fd943e5848910ae3151
Mango <phankhoatn2011@gmail.com>
2025-03-26 02:06:20 +0700

    Update EmployeeRepository.java

--- src/main/java/com/mycompany/LeaveManagementSystem/repository/EmployeeRepository.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/repository/EmployeeRepository.java
@@ -6,7 +6,6 @@ package com.mycompany.LeaveManagementSystem.repository;
 
 import com.mycompany.LeaveManagementSystem.model.Employees;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -14,12 +13,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author phank
  */
 public interface EmployeeRepository extends JpaRepository<Employees, Integer> {
-     public Employees findByEmail(String email);
-
-    public Optional<Employees> findById(Long employeeId);
-    
-    List<Employees> findByManagerId(int managerId);
-    List<Employees> findByEmployee(Employees employee);
-    List<Employees> findByManager(Employees manager);
-
+     List<Employees> findByManager(Employees manager);
+List<Employees> findByManagerEid(int eid);
 }

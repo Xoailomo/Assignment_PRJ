c3fd8414a09a56dfcd99c085ace8ab3f18ac7c6c
Mango <phankhoatn2011@gmail.com>
2025-03-25 23:34:08 +0700

    Update Features.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/Features.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/Features.java
@@ -4,10 +4,7 @@
  */
 package com.mycompany.LeaveManagementSystem.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 /**

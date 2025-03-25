231331287a80a36480f4e9ba26462839b407c130
Mango <phankhoatn2011@gmail.com>
2025-03-25 19:03:13 +0700

    Update Features.java

--- src/main/java/com/mycompany/LeaveManagementSystem/model/Features.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/Features.java
@@ -4,17 +4,26 @@
  */
 package com.mycompany.LeaveManagementSystem.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 
 /**
  *
  * @author phank
  */
+@Getter
+@Setter
 @Entity
 @Table(name="features")
 public class Features {
-    @
+    @Id
     private int fid;
     
+    @Column(nullable = false, length = 150)
+    private String url;
+
+  
 }

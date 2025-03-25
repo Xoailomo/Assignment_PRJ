4261de98c203c506135d7670af2f1e53d9bd599c
Mango <phankhoatn2011@gmail.com>
2025-03-25 18:59:30 +0700

    update model name in role repo

--- src/main/java/com/mycompany/LeaveManagementSystem/repository/RoleRepository.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/repository/RoleRepository.java
@@ -1,9 +1,9 @@
 package com.mycompany.LeaveManagementSystem.repository;
 
-import com.mycompany.LeaveManagementSystem.model.Role;
+import com.mycompany.LeaveManagementSystem.model.Roles;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    Optional<Role> findByName(String name); 
+public interface RoleRepository extends JpaRepository<Roles, Integer> {
+    Optional<Roles> findByName(String name); 
 }

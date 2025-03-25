da1bc3b502bc374cf299dc955541d5ee7dfd9012
Mango <phankhoatn2011@gmail.com>
2025-03-25 19:02:18 +0700

    Update Users.java
    use lombok for getter and setter
    update variable

--- src/main/java/com/mycompany/LeaveManagementSystem/model/Users.java
+++ src/main/java/com/mycompany/LeaveManagementSystem/model/Users.java
@@ -7,76 +7,26 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 public class Users {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @Column(length = 150)
     private String username;
+    
+    @Column(nullable = false, length = 150)
     private String password;
-    private String displayName;
-    @Column(nullable = true)
-    private int eid;
-
-    public int getEid() {
-        return eid;
-    }
-
-    public void setEid(int eid) {
-        this.eid = eid;
-    }
     
-//    private String firstname;
-//    private String lastname;
-//    private String email;
-//    private String role;
-//    private String approverName;
-//    private String team;
-//    private String office;
-//    private String country;
-//    private Date createAt;
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-     
-
-    @OneToOne
-    @JoinColumn(name = "employee_id", referencedColumnName = "id")
-    private Employee employee;
-
-    @Transient
-    private List<Users> approvers;
-
-    @Transient
-    private List<WorkingDay> workingDays;
-
-    public Users() {
-    }
-
-//    public Users(String firstname, String lastname) {
-//        this.firstname = firstname;
-//        this.lastname = lastname;
-//    }
-//
-//    public Date getCreateAt() {
-//        return createAt;
-//    }
-//
-//    public void setCreateAt(Date createAt) {
-//        this.createAt = createAt;
-//    }
+    @Column(nullable = false, length = 150)
+    private String displayname;
     
+    @ManyToOne
+    @JoinColumn(name = "eid")
+    private Employees employee;
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String userName) {
-        this.username = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
@@ -87,100 +37,21 @@ public class Users {
         this.password = password;
     }
 
-    // GETTERS & SETTERS
-    public int getId() {
-        return id;
+    public String getDisplayname() {
+        return displayname;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
     }
 
-//    public String getFirstname() {
-//        return firstname;
-//    }
-//
-//    public void setFirstName(String firstname) {
-//        this.firstname = firstname;
-//    }
-//
-//    public String getLastname() {
-//        return lastname;
-//    }
-//
-//    public void setLastName(String lastname) {
-//        this.lastname = lastname;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-
-//    public void setEmail(String email) {
-//        this.email = email;
-//    }
-//
-//    public String getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(String role) {
-//        this.role = role;
-//    }
-//
-//    public String getApproverName() {
-//        return approverName;
-//    }
-//
-//    public void setApproverName(String approverName) {
-//        this.approverName = approverName;
-//    }
-//
-//    public String getTeam() {
-//        return team;
-//    }
-//
-//    public void setTeam(String team) {
-//        this.team = team;
-//    }
-//
-//    public String getOffice() {
-//        return office;
-//    }
-
-//    public void setOffice(String office) {
-//        this.office = office;
-//    }
-//
-//    public String getCountry() {
-//        return country;
-//    }
-//
-//    public void setCountry(String country) {
-//        this.country = country;
-//    }
-
-    public Employee getEmployee() {
+    public Employees getEmployee() {
         return employee;
     }
 
-    public void setEmployee(Employee employee) {
+    public void setEmployee(Employees employee) {
         this.employee = employee;
     }
-
-    public List<Users> getApprovers() {
-        return approvers;
-    }
-
-    public void setApprovers(List<Users> approvers) {
-        this.approvers = approvers;
-    }
-
-    public List<WorkingDay> getWorkingDays() {
-        return workingDays;
-    }
-
-    public void setWorkingDays(List<WorkingDay> workingDays) {
-        this.workingDays = workingDays;
-    }
+    
+    
 }
